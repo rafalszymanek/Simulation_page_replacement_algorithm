829eea147665cf192639de3803c426b27b667d4f
Rafal Szymanek <rszymanek1997@gmail.com>
2019-01-11 19:58:51 +0100

    Ready algorythm LRU for bigger input data

--- sample/LRU_algorythm.py
+++ sample/LRU_algorythm.py
@@ -4,30 +4,35 @@ from sample.frames import Frames
 
 class LruAlgorythm:
     def __init__(self, number):
+        self.number = number
         self.ramka = Frames(number)
-        self.queueOfPages = [1,2,3,4,1,2,5,1,2,3]
         self.listOfPages = []
         self.attempt = 1
 
-    def doAlgorythm(self):
+    def doAlgorythm(self, queueOfPages):
+        self.ramka = Frames(self.number)
+        self.listOfPages = []
+        self.attempt = 1
         for i in range(21): # To easiest way index 0 will be never used. !20! is max value
             self.listOfPages.append(PageLRU(i))
 
-        for page in self.queueOfPages:
-            print ("Ramki: " + str(self.ramka.listOfFrames))
-            print ("Chce wejść: " + str(page))
+        for page in queueOfPages:
+            # print ("Ramki: " + str(self.ramka.listOfFrames))
+            # print ("Chce wejść: " + str(page))
 
             self.usePage(page)
             if self.ramka.checkIsPagesIdInFrames(page):
-                print("Strona znajduje się w ramce!")
+                # print("Strona znajduje się w ramce!")
+                pass
             else:
                 indexPageToChange = self.findLeastFrequently()
-                print("Ineks do zastąpienia: " + str(indexPageToChange))
+                # print("Ineks do zastąpienia: " + str(indexPageToChange))
                 self.ramka.addPageIdToFrame(page, indexPageToChange)
                 self.ramka.addPageFault()
-            print ("Ramki po zmianie: " + str(self.ramka.listOfFrames) + "\n\n")
+            # print ("Ramki po zmianie: " + str(self.ramka.listOfFrames) + "\n\n")
 
-        print(self.ramka.pageFault)
+        # print(self.ramka.pageFault)
+        return self.ramka.pageFault
 
 
 
--- simulation.py
+++ simulation.py
@@ -1,5 +1,33 @@
 from sample.LRU_algorythm import *
+from sample.open_data import *
+from statistics import mean
+
+path = "data/test_values.txt"
+matrix = openFileAndPutIntoMatrix(path)
+
+width, height = checkWidthAndHeightOfFile(path)
+lruResult3 = []
 
 algorytm = LruAlgorythm(3)
+for row in range(height):
+    lruResult3.append(algorytm.doAlgorythm(matrix[row]))
 
-algorytm.doAlgorythm()
+
+print ("------------\nLRU\n------------\n")
+print ("Srednia dla tablicy 3 elementowej: "+str(mean(lruResult3)))
+
+lruResult5 = []
+
+algorytm = LruAlgorythm(5)
+for row in range(height):
+    lruResult5.append(algorytm.doAlgorythm(matrix[row]))
+
+print ("Srednia dla tablicy 5 elementowej: "+str(mean(lruResult5)))
+
+lruResult7 = []
+
+algorytm = LruAlgorythm(7)
+for row in range(height):
+    lruResult7.append(algorytm.doAlgorythm(matrix[row]))
+
+print ("Srednia dla tablicy 7 elementowej: "+str(mean(lruResult7)))
