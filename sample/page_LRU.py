7a5f3ecb14af8bcbfaf12aff99a2dcec2a40b261
Rafal Szymanek <rszymanek1997@gmail.com>
2019-01-11 18:15:52 +0100

    Change in page page_LRU class concept of attempt

--- sample/page_LRU.py
+++ sample/page_LRU.py
@@ -1,16 +1,13 @@
 class PageLRU:
     def __init__(self, id):
         self.id = id
-        self.pageOld = 0
+        self.numberOfAtempt = 0
 
     def checkId(self):
         return self.id
 
-    def usePage(self):
-        self.pageOld = 0
+    def usePage(self, attempt):
+        self.numberOfAtempt = attempt
 
-    def incresePageOld(self):
-        self.pageOld+=1
-
-    def checkPageOld(self):
-        return self.pageOld
+    def checkNumberOfAtempt(self):
+        return self.numberOfAtempt
